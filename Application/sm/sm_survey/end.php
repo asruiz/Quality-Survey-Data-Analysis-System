0f73346dd8b94f70a763f1a7298ce879506903be
Diego Gloria <diegogloria14@gmail.com>
2017-04-10 14:44:51 +0800

    Added modals for confirmation message

--- Application/sm/sm_survey/end.php
+++ Application/sm/sm_survey/end.php
@@ -1,5 +1,76 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
 
-echo 'Thank you!';
+<head>
 
-?>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="">
+	<meta name="author" content="">
+
+	<title>Taal Vista | Success </title>
+
+	<!-- Bootstrap Core CSS -->
+	<link href="assets/css/bootstrap.min.css" rel="stylesheet">
+
+	<!-- Custom CSS -->
+	<link href="assets/css/stylish-portfolio.css" rel="stylesheet">
+
+	<!-- Custom Fonts -->
+	<link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+	<link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
+
+	<style type="text/css">
+		.logbox {
+			margin: 0 auto;
+			overflow: hidden;
+			background-color: #fff;
+			border: 4px solid #B48A00; 
+			width: 900px;
+			height: 500px;
+			-webkit-box-shadow: 0 1px 5px rgba(0, 0, 0, 0.50);
+			-moz-box-shadow: 0 1px 5px rgba(0, 0, 0, 0.50);
+			box-shadow: 0 1px 5px rgba(0, 0, 0, 0.50);
+		}
+
+		.hr-line {
+            color: #fff;
+            box-shadow: 2px 2px 5px rgba(180,138,0,0.4);
+            border: 1px solid #B48A00;
+            width:80%;
+        }
+	</style>
+
+	<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+        <![endif]-->
+
+    </head>
+
+    <body style="background: url(assets/img/taal-vista.jpg) no-repeat center center scroll">
+    	<!-- Header -->
+    	<div style="position:absolute; top:50px; left:230px;">
+	    	<header id="top" class="header logbox">
+	    		<div class="text-vertical-center">
+	    		<img src="assets/img/logo.png">
+	    		<br/><br/><br/><br/>
+		    		<h3>Successfully Submitted</h3>
+		    		<hr class="hr-line">
+	    			<p>Thank you for answering our survey! Your response will help us serve you better.</p>
+	    			<p style="text-align: right; padding-right: 25%;">General Manager</p>
+	    			<br/><br/><br/><br/><br/><br/>
+	    		</div>
+	    	</header>
+    	</div>
+
+
+    	<!-- jQuery -->
+    	<script src="assets/js/jquery.js"></script>
+    	<!-- Bootstrap Core JavaScript -->
+    	<script src="assets/js/bootstrap.min.js"></script></body>
+    </body>
+</html>
--- Application/sm/sm_survey/home.php
+++ Application/sm/sm_survey/home.php
@@ -96,20 +96,20 @@ if($_POST['btn_submit'])
     $dbh_pre_reg_request_header =  cobalt_load_class('survey_header');
     $dbh_pre_reg_request_header->execute_query("SELECT COUNT(survey_header_id) AS req_id FROM survey_header");
 
-  $result = $dbh_pre_reg_request_header->result;
-  $row = $result->fetch_assoc();
+    $result = $dbh_pre_reg_request_header->result;
+    $row = $result->fetch_assoc();
 
-  extract($row);
+    extract($row);
 
 
-  if($row == '')
-  {
-    $counter = 1;
-  }
-  else
-  {
-    $counter = ++$req_id;
-  }
+    if($row == '')
+    {
+        $counter = 1;
+    }
+    else
+    {
+        $counter = ++$req_id;
+    }
     $req_tag = 'SN';
     $req_count = str_pad($counter, '5','0', STR_PAD_LEFT);
     $code = "{$req_tag}".date('Y')."-{$req_count}";
@@ -130,7 +130,7 @@ if($_POST['btn_submit'])
             );
         $dbh->add($param);
         $survey_header_id = $dbh->auto_id;
-         $dbh = cobalt_load_class('survey_details');
+        $dbh = cobalt_load_class('survey_details');
 
         /* Ratings Submit */
         for($a = 0;$a <count($_POST['ratings']);++$a)
@@ -210,6 +210,7 @@ if($_POST['btn_submit'])
                 $dbh->add($parameters); 
             }
         }
+        redirect('end.php');
     }
 
     else {
@@ -221,124 +222,137 @@ if($_POST['btn_submit'])
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>Taal Vista Survey Form</title>
+    <title>Taal Vista | Survey</title>
     <link href="assets/css/bootstrap.min.css" rel="stylesheet">
     <link href="assets/css/stylish-portfolio.css" rel="stylesheet">
     <link href="assets/css/taal-vista.css" rel="stylesheet">
+    <link href="assets/css/input-text.css" rel="stylesheet">
+    <style type="text/css">
+        body {
+            background-color: gold; 
+            background: url(assets/img/taal-vista.jpg); 
+            background-size: 100% 100%; 
+            background-attachment: fixed; 
+            background-repeat: no-repeat;
+        }
 
-    <style>
-    #overlay 
-    {
-        position:fixed;
-        top:1px;
-        left:1px;
-        height:100%;
-        width:100%;
-        visibility:hidden;
-        background-color:rgba(255,255,255,.4);
-    }
+        .logbox {
+            margin: 0 auto;
+            margin-top: 1%;
+            margin-bottom: 5%;
+            overflow: hidden;
+            background-color: #fff;
+            border: 4px solid #B48A00;
+            border-radius: 25px;
+            width: 800px;
+            height: auto;
+            -webkit-box-shadow: 0 1px 5px rgba(0, 0, 0, 0.25);
+            -moz-box-shadow: 0 1px 5px rgba(0, 0, 0, 0.25);
+            box-shadow: 0 1px 5px rgba(0, 0, 0, 0.25);
+        }
+        .hr-line {
+            color: #fff;
+            box-shadow: 2px 2px 5px rgba(180,138,0,0.4);
+            border: 1px solid #B48A00;
+            width:80%;
+        }
+        
+        .guest-info-container {
+            padding-left: 10%; 
+            margin-top: -4%;
+        } 
 
-    .modal_content
-    {
-       height:300px;
+        #overlay 
+        {
+            position:fixed;
+            top:1px;
+            left:1px;
+            height:100%;
+            width:100%;
+            visibility:hidden;
+            background-color:rgba(255,255,255,.4);
+        }
 
-    }
+        .modal_content
+        {
+         height:300px;
 
-    .header
-    {
-        border-bottom-left-radius:0px;
-        border-bottom-right-radius:0px;
-    }
-
-    .container
-    {
-        border-bottom-left-radius:0px;
-        border-bottom-right-radius:0px;
-        border-top-left-radius:0px;
-        border-top-right-radius:0px;
-    }
-
-    .footer
-    {
-        border-top-left-radius:0px;
-        border-top-right-radius:0px;
-    }
-  
+     }
     </style>
-
 </head>
 
-<body style="background-color: gold; background: url(assets/img/taal-vista.jpg); background-size: 100% 100%; background-attachment: fixed; background-repeat: no-repeat;">
-
-<div class="logbox">
-<img src="assets/img/logo.png">
-
-<?php
-require_once '../sm_survey_admin/core/subclasses/user_html.php';
-$html = new user_html;
-
-if($message != "")
-{
-    $html->draw_header();
-    $html->display_error($message);
-
-}
-
-?>
-
-<form method="POST" action="home.php">
-    <!-- Table for Guest Information -->
-    <div id = "overlay">
-        <div class="logbox">
+<body>
+    <div class="logbox">
+        <img src="assets/img/logo.png">
             <div align="center">
-               <br/><img src = "assets/img/warning.png" style="width: 100px; height: 100px;"><br/>
+                <h3>Customer Survey</h3>
+                <hr class="hr-line">
+            </div>
+        <?php
+        require_once '../sm_survey_admin/core/subclasses/user_html.php';
+        $html = new user_html;
 
-               <h2>Are you sure?</h2>
+        if($message != "")
+        {
+            $html->draw_header();
+            $html->display_error($message);
+        }
+        ?>
 
-               <input type="submit" class="btn btn-lg" style="background-color: #B48A00; color: white;" name="btn_submit"  value="Yes">
-               <span class="btn btn-lg" style="background-color: #B48A00; color: white;" onclick="hide_modal()">No</span>
+        <form method="POST" action="home.php">
+            <!-- Table for Guest Information -->
+            <div id = "overlay">
+                <div class="logbox">
+                    <div align="center">
+                       <br/><img src = "assets/img/warning.png" style="width: 100px; height: 100px;"><br/>
+
+                       <h2>Are you sure?</h2>
+
+                       <input type="submit" class="btn btn-lg" style="background-color: #B48A00; color: white;" name="btn_submit"  value="Yes">
+                       <span class="btn btn-lg" style="background-color: #B48A00; color: white;" onclick="hide_modal()">No</span>
+                   </div>
+                   <br/>
+               </div>
            </div>
-           <br/>
-       </div>
-   </div>
-    <br>
-    <table align="center">
-        <tbody>
-            <tr>
-                <?php echo '<td>First Name: <input class="form-control form-inline" type="text" name="guest_first_name"></td>';?>
-                <td> &nbsp; &nbsp; </td>
-                <?php echo '<td>Last Name: <input class="form-control form-inline" type="text" name="guest_last_name"></td>';?>
-            </tr>
+           <br>
+           <div class ="guest-info-container">
+            <table>
+                <tbody>
+                    <tr>
+                        <?php echo '<td><input class="input form-inline" type="text" name="guest_first_name" placeholder="First Name"></td>';?>
+                        <td> &nbsp; &nbsp; </td>
+                        <?php echo '<td><input class="input form-inline" type="text" name="guest_last_name" placeholder="Last Name"></td>';?>
+                    </tr>
 
-            <tr>
-                <?php echo '<td>Room Number: <input class="form-control form-inline" type="text" name="room_number"></td>';?>
-                <td> &nbsp; &nbsp; </td>
-                <?php echo '<td>Check-out date: <input class="form-control form-inline" type="date" name="guest_check_out"></td>';?>
-            </tr>
-        </tbody>
-    </table>
+                    <tr>
+                        <?php echo '<td><input class="input form-inline" type="text" name="room_number" placeholder="Room Number"></td>';?>
+                        <td> &nbsp; &nbsp; </td>
+                        <td><input class="input form-inline" placeholder="Check-out Date" type="text" onfocus="(this.type='date')" onblur="(this.type='text')" <?php echo 'name="guest_check_out"'?>></td>
+                    </tr>
 
-    <br/>
+                </tbody>
+            </table>
+        </div>
 
-    <!-- Table for Scorecard -->
-    <table class="table table-borderless" align="center">
-        <thead>
-            <tr>
-              <th style="width:50%;">  </th>
-              <th>Excellent</th>
-              <th>Very<br/> Good</th>
-              <th>Good</th>
-              <th>Fair</th>
-              <th>Poor</th>
-          </tr>
-      </thead>
+        <!-- Table for Scorecard -->
+        <table class="table table-borderless" align="center">
+            <thead>
+                <tr>
+                  <th style="width:50%;">  </th>
+                  <th>Excellent</th>
+                  <th>Very<br/> Good</th>
+                  <th>Good</th>
+                  <th>Fair</th>
+                  <th>Poor</th>
+              </tr>
+          </thead>
 
-        <tbody>
+          <tbody>
             <?php
             for($a = 0;$a < count($arr_result);++$a)
             {
              ?>
-            <tr>
+             <tr>
                 <td style="font-weight: bold;"><?php echo $arr_result[$a]['question_header_description']?></td>
 
                 <?php
@@ -348,7 +362,7 @@ if($message != "")
                     {
                         echo '<td></td><td></td><td></td><td></td><td></td><tr>';
                         echo '<td style="font-style: italic;">'. $arr_result[$a]['questions'][$b]['question_details_description'].'</td>';
-                        echo '<td align="center"><input type="radio" name="ratings['.$a.']['.$b.']" value="5"';
+                        echo '<td align="center"><input type="radio" class="radio" name="ratings['.$a.']['.$b.']" value="5"';
                         if(isset($_POST['ratings'][$a][$b]) && $_POST['ratings'][$a][$b] == '5')
                         { 
                             echo 'checked="checked"';
@@ -389,52 +403,52 @@ if($message != "")
 
                 else
                 {
-                     echo '<td align="center"><input type="radio" name="ratings['.$a.']" value="5"';
-                        if(isset($_POST['ratings'][$a]) && $_POST['ratings'][$a] == '5')
-                        { 
-                            echo 'checked="checked"';
-                        }
-                        echo '></td>';
-
-                        echo '<td><input type="radio" name="ratings['.$a.']" value="4"';
-                        if(isset($_POST['ratings'][$a]) && $_POST['ratings'][$a] == '4')
-                        { 
-                            echo 'checked="checked"';
-                        }
-                        echo '></td>';
-
-                        echo '<td><input type="radio" name="ratings['.$a.']" value="3"';
-                        if(isset($_POST['ratings'][$a]) && $_POST['ratings'][$a]== '3')
-                        { 
-                            echo 'checked="checked"';
-                        }
-                        echo '></td>';
-
-                        echo '<td><input type="radio" name="ratings['.$a.']" value="2"';
-                        if(isset($_POST['ratings'][$a]) && $_POST['ratings'][$a] == '2')
-                        { 
-                            echo 'checked="checked"';
-                        }
-                        echo '></td>';
-
-                        echo '<td><input type="radio" name="ratings['.$a.']" value="1"';
-                        if(isset($_POST['ratings'][$a]) && $_POST['ratings'][$a] == '1')
-                        { 
-                            echo 'checked="checked"';
-                        }
-                        echo '></td>';
-                    echo '</tr>';
+                 echo '<td align="center"><input type="radio" name="ratings['.$a.']" value="5"';
+                 if(isset($_POST['ratings'][$a]) && $_POST['ratings'][$a] == '5')
+                 { 
+                    echo 'checked="checked"';
                 }
-                ?>
-                <?php        
-            }        
-            ?>
-        </tbody>
-    </table> 
+                echo '></td>';
 
-    <!-- Table for Checkbox -->
-    <table class="table table-borderless" align="center">
-        <tbody>
+                echo '<td><input type="radio" name="ratings['.$a.']" value="4"';
+                if(isset($_POST['ratings'][$a]) && $_POST['ratings'][$a] == '4')
+                { 
+                    echo 'checked="checked"';
+                }
+                echo '></td>';
+
+                echo '<td><input type="radio" name="ratings['.$a.']" value="3"';
+                if(isset($_POST['ratings'][$a]) && $_POST['ratings'][$a]== '3')
+                { 
+                    echo 'checked="checked"';
+                }
+                echo '></td>';
+
+                echo '<td><input type="radio" name="ratings['.$a.']" value="2"';
+                if(isset($_POST['ratings'][$a]) && $_POST['ratings'][$a] == '2')
+                { 
+                    echo 'checked="checked"';
+                }
+                echo '></td>';
+
+                echo '<td><input type="radio" name="ratings['.$a.']" value="1"';
+                if(isset($_POST['ratings'][$a]) && $_POST['ratings'][$a] == '1')
+                { 
+                    echo 'checked="checked"';
+                }
+                echo '></td>';
+                echo '</tr>';
+            }
+            ?>
+            <?php        
+        }        
+        ?>
+    </tbody>
+</table> 
+
+<!-- Table for Checkbox -->
+<table class="table table-borderless" align="center">
+    <tbody>
         <?php
         for($c = 0;$c < count($arr_result_checkbox);++$c)
         {
@@ -463,7 +477,6 @@ if($message != "")
                         value="'.$arr_result_checkbox[$c]['questions'][$d]['question_details_description'].'" 
                         '.$checked.'>
                         &nbsp;'.$arr_result_checkbox[$c]['questions'][$d]['question_details_description'].'</td>';
-                            // echo '<td style="font-style: italic;"></td>';
                         echo '</tr>';
 
                     } 
@@ -483,8 +496,7 @@ if($message != "")
             {
                 ?>
                 <tr>
-                    <td style="font-weight: bold;"><?php echo $arr_result_feedback[$e]['question_header_description']?></td>
-                    
+                    <td style="font-weight: bold"><?php echo $arr_result_feedback[$e]['question_header_description']?></td>
                     <?php
                     if(isset($arr_result_feedback[$e]['questions']))
                     {
@@ -500,27 +512,27 @@ if($message != "")
                                 $textarea_value = "";
                             }
                             if($f == 2) {    
-                                echo '<td>&nbsp;'. $arr_result_feedback[$e]['questions'][$f]['question_details_description'].'<br/><textarea rows = "5" cols = "50" name="feedback['.$f.']" placeholder="Name of Staff">'.$textarea_value.'</textarea></td>';
+                                echo '<td>&nbsp;'. $arr_result_feedback[$e]['questions'][$f]['question_details_description'].'<br/><textarea rows = "5" cols = "50" name="feedback['.$f.']" placeholder="Name of Staff" class="input-textarea">'.$textarea_value.'</textarea></td>';
                             }
 
                             else {
-                                echo '<td>&nbsp;'. $arr_result_feedback[$e]['questions'][$f]['question_details_description'].'<br/><textarea rows = "5" cols = "50" name="feedback['.$f.']">'.$textarea_value.'</textarea></td>';
-                            }
+                                echo '<td>&nbsp;'. $arr_result_feedback[$e]['questions'][$f]['question_details_description'].'<br/><textarea rows = "5" cols = "50" name="feedback['.$f.']" class="input-textarea   ">'.$textarea_value.'</textarea></td>';
+                            }   
                             
                             echo '</tr>';
                         } 
                     }
-                ?>
-                <?php        
-            }        
-            ?>         
-        </tbody>
-    </table>    
-</form>
-<div align="center">
-    <button class="btn btn-lg" style="background-color: #B48A00; color: white;" onclick="show_modal()">Submit</button>
-    <br><br>
-</div>
+                    ?>
+                    <?php        
+                }        
+                ?>         
+            </tbody>
+        </table>    
+    </form>
+    <div align="center">
+        <button class="btn btn-lg" style="background-color: #B48A00; color: white;" onclick="show_modal()">Submit</button>
+        <br><br>
+    </div>
 </div>
 
 </body>
